7fa9ae6888b9ca8e53ede2191f528326e5e20c24
ragav21 <ragav.21@gmail.com>
2024-06-20 11:17:43 +0530

    updates

---
+++ Applications/main.tf
@@ -0,0 +1,13 @@
+module "Storage" {
+    source = "../Modules/Storage/storage-account"
+     hns = false
+ infra_encryption = false
+ location = "westus"
+ resource_group_name = "raghavanrg"
+instance_number = "001"
+enable_versioning = true
+app_prefix = "jm"
+environment = "dev"
+    
+
+}
